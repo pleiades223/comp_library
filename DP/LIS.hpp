f91c4fecdf27c2ad6112dfbdbcc879ac50b4a5f8
pleiades_223 <pleiades.lab223@gmail.com>
2024-02-25 22:18:52 +0900

    add LIS

---
+++ DP/LIS.hpp
@@ -0,0 +1,18 @@
+#pragma once
+/*
+最長増加部分列
+O( N log N )
+*/
+#include<vector>
+
+template<typename T>
+std::vector<int> LIS(std::vector<T> &vec){
+    std::vector<int> ans;
+    ans.reserve((int)vec.size());
+    for(int i=0;i<vec.size();i++){
+        auto itr = std::lower_bound(ans.begin(),ans.end(),vec[i]);
+        if(itr==ans.end())ans.push_back(vec[i]);
+        else *itr=vec[i];
+    }
+    return ans;
+}
