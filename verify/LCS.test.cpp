266e5ad853975aefcaaec0be1b1beb4ce2cf2419
pleiades_223 <pleiades.lab223@gmail.com>
2024-02-25 21:55:54 +0900

    fix LCS.test

--- verify/LCS.test.cpp
+++ verify/LCS.test.cpp
@@ -1,14 +1,15 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_10_C"
+using namespace std;
 #include <string>
 #include <iostream>
 #include <DP/LCS.hpp>
 
 int main(){
     int q;
-    std::cin >> q;
+    cin >> q;
     while(q--){
-        std::string X,Y;
-        std::cin >> X >> Y;
-        std::cout << LCS(X,Y) << std::endl;
+        string X,Y;
+        cin >> X >> Y;
+        cout << LCS(X,Y) << endl;
     }
 }
