154943d2dd2d3a383c570ef680e87066e940c068
pleiades_223 <pleiades.lab223@gmail.com>
2024-02-25 22:19:06 +0900

    fix LCS

--- DP/LCS.hpp
+++ DP/LCS.hpp
@@ -8,12 +8,12 @@ O( |A| x |B| )
 
 template <typename T>
 int LCS(T a,T b){
-    vector<vector<int>> dp(a.size()+1,vector<int>(b.size()+1,0));
+    std::vector<std::vector<int>> dp(a.size()+1,std::vector<int>(b.size()+1,0));
     for(int i=0;i<a.size();i++){
         for(int j=0;j<b.size();j++){
-            dp[i+1][j+1]=max(dp[i+1][j],dp[i][j+1]);
+            dp[i+1][j+1]=std::max(dp[i+1][j],dp[i][j+1]);
             if(a[i]==b[j]){
-                dp[i+1][j+1]=max(dp[i+1][j+1],dp[i][j]+1);
+                dp[i+1][j+1]=std::max(dp[i+1][j+1],dp[i][j]+1);
             }
         }
     }
