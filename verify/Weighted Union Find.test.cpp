514096a47e8a6015a362bd7e7318b14feada50eb
pleiades_223 <pleiades.lab223@gmail.com>
2024-07-07 00:39:25 +0900

    fix weighted union find

--- verify/Weighted Union Find.test.cpp
+++ verify/Weighted Union Find.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/1/DSL_1_B"
 #include <iostream>
-#include <graph/Weighted Union Find.hpp>
+#include <graph/Weighted-Union-Find.hpp>
 
 int main(){
     int n, q;
